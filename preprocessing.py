7843431506ebfa3d8cae2177f8f73043faacab1b
FrankYan93 <yanjiadong@brandeis.edu>
2017-04-26 14:19:17 -0400

    input_wrapper

--- preprocessing.py
+++ preprocessing.py
@@ -5,11 +5,32 @@ from os.path import isfile, join
 import re
 from PIL import Image
 import numpy as np
+from scipy import misc
+from skimage.morphology import binary_dilation,dilation,disk
 
 sh = shelve.open("training",writeback=False)
 imgFolderPath = getcwd() + sep + "annotated"
 files = [f for f in listdir(imgFolderPath) if isfile(join(imgFolderPath, f)) and imghdr.what(join(imgFolderPath, f))=='png']
 counter = 0
+
+def input_wrapper(f):
+	image = misc.imread(f)
+	sx,sy = image.shape
+	diff = np.abs(sx-sy)
+
+	sx,sy = image.shape
+	image = np.pad(image,((sx//8,sx//8),(sy//8,sy//8)),'constant')
+	if sx > sy:
+		image = np.pad(image,((0,0),(diff//2,diff//2)),'constant')
+	else:
+		image = np.pad(image,((diff//2,diff//2),(0,0)),'constant')
+
+	image = dilation(image,disk(max(sx,sy)/32))
+	image = misc.imresize(image,(32,32))
+	if np.max(image) > 1:
+		image = image/255.
+	return image
+
 for e in files:
     h = re.match(".*eq\d*_(.*)_(\d*)_(\d*)_(\d*)_(\d*)\.png",e)
     if h is None:
@@ -19,19 +40,10 @@ for e in files:
         counter+=1
         tmp = []
         symbol = h.group(1)
-        # y1 = h.group(2)
-        # y2 = h.group(3)
-        # x1 = h.group(4)
-        # x2 = h.group(5)
-        with open(join(imgFolderPath, e), 'r+b') as f:
-            with Image.open(f) as img_opened:
-                img = np.array(img_opened)
-                tmp.append(img)
+        img = input_wrapper(join(imgFolderPath, e))
+        misc.imsave("example.png",img)
+        tmp.append(img)
         tmp.append(symbol)
-        # tmp.append(y1)
-        # tmp.append(y2)
-        # tmp.append(x1)
-        # tmp.append(x2)
         sh[str(counter)] = tmp
         #print tmp
 sh.close()
