980ce562e52d2c8d37ea7dbd481ecf62dd2c969d
FrankYan93 <yanjiadong@brandeis.edu>
2017-04-27 20:52:14 -0400

    general idea od partition

--- partition.py
+++ partition.py
@@ -5,6 +5,19 @@
 # output lst
 # ----------
 # list :  tuple of format (symbol, x1, y1, x2, y2)
+
+class Partition(object):
+    def __init__(self,mst):
+        self.mst = mst
+        self.lst = []
+
+    def getList():
+        return self.lst
+
+    def generateList():
+        pass
+
+
 root = -1
 value = []
 for e in mst:
