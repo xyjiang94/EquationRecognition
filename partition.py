921db4d9648182219453af98939c53d7beab9419
FrankYan93 <yanjiadong@brandeis.edu>
2017-04-27 20:49:59 -0400

    general idea od partition

--- partition.py
+++ partition.py
@@ -1,1 +1,15 @@
 # TODO
+# input mst
+# ----------
+# dictionary :  key = vertex, value = a list of tuples (tuple: [connected vertex, weight])
+# output lst
+# ----------
+# list :  tuple of format (symbol, x1, y1, x2, y2)
+root = -1
+value = []
+for e in mst:
+    root = e
+    value = mst[e]
+    break
+# dp func
+# f(v) =
