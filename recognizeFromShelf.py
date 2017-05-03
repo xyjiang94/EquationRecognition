279d73bdaaeddfabad0b501ed92759b4dab9962e
FrankYan93 <yanjiadong@brandeis.edu>
2017-05-03 15:39:02 -0400

    recognizeFromShelf

--- recognizeFromShelf.py
+++ recognizeFromShelf.py
@@ -1,13 +1,13 @@
-import imghdr  # recognize img type
-from os import listdir, getcwd, sep
-from os.path import isfile, join
+import shelve
+import numpy as np
+import json
 from subprocess import call
 import re
-import json
 import tensorflow as tf
 from scipy import misc
-import numpy as np
 from MER_NN import SymbolRecognition, input_wrapper
+from os.path import isfile, join
+from os import listdir, getcwd, sep
 
 symMap = {}
 with open('symbol_mapping.json', 'r') as opened:
@@ -15,9 +15,7 @@ with open('symbol_mapping.json', 'r') as opened:
 print symMap
 
 model_path = join(getcwd(), "model", "model.ckpt")
-imgFolderPath = getcwd() + sep + "annotated"
-files = [f for f in listdir(imgFolderPath) if isfile(
-    join(imgFolderPath, f)) and imghdr.what(join(imgFolderPath, f)) == 'png']
+sh = shelve.open("training",writeback=False)
 
 with tf.Session() as sess:
     sr = SymbolRecognition(sess, model_path, trainflag=False)
@@ -26,25 +24,13 @@ with tf.Session() as sess:
     count = 0
     acc = 0
     fid = 0
-    for f in files:
-        # recognize files
-        the_file = f
-        g = re.match(".*eq\d*_(.*)_\d*_\d*_\d*_\d*.png", the_file)
-        if g:
-            res = g.group(1)
-        else:
-            continue
-        f = join(imgFolderPath, f)
-        count += 1
-        image = input_wrapper(f)
-        # test = sr.pr(image)
-        # print "test: ",test
-        # print sess.run(tf.nn.softmax(test),dim=-1)
+    for i in xrange(1,1000):
+        data = sh[str(i)]
+        image = data[0]
+        res = data[1]
         p = sr.p(image)
         print "predict: ",p
         p = symMap[str(p[0])]
-        print the_file
-
         print "res: ",res,type(res)
         print "p: ", p,type(p)
         misc.imsave('trans/' + str(p) + '_' + res + ".png", np.reshape(image, (32, 32)))
@@ -54,6 +40,4 @@ with tf.Session() as sess:
             misc.imsave('err/' + str(p) + '_' + res + ".png",
                         np.reshape(image, (32, 32)))
         fid += 1.
-        if count >= 1000:
-            break
     print(acc, fid, acc / fid)
