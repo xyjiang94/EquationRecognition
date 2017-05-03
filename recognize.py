be051070645a583d89f1c91a75daf2b83c1c7246
FrankYan93 <yanjiadong@brandeis.edu>
2017-05-03 13:58:43 -0400

    recognize demo

--- recognize.py
+++ recognize.py
@@ -1,4 +1,3 @@
-from MER_NN import SymbolRecognition
 import imghdr  # recognize img type
 from os import listdir, getcwd, sep
 from os.path import isfile, join
@@ -6,12 +5,16 @@ from subprocess import call
 import re
 import json
 import tensorflow as tf
+from scipy import misc
+import numpy as np
+from MER_NN import SymbolRecognition, input_wrapper
 
 symMap = {}
 with open('symbol_mapping.json', 'r') as opened:
     symMap = json.loads(opened.read())
+print symMap
 
-model_path = getcwd() + sep + "model"
+model_path = join(getcwd(), "model", "model.ckpt")
 imgFolderPath = getcwd() + sep + "annotated"
 files = [f for f in listdir(imgFolderPath) if isfile(
     join(imgFolderPath, f)) and imghdr.what(join(imgFolderPath, f)) == 'png']
@@ -26,20 +29,28 @@ with tf.Session() as sess:
     for f in files:
         # recognize files
         # do something
+        the_file = f
         f = join(imgFolderPath, f)
         count += 1
         image = input_wrapper(f)
         p = sr.p(image)
-        p = symMap[p[0]]
-        g = re.match(".*eq\d*_(.*)_\d*_\d*_\d*_\d*.png", f)
-        res = g.group(1)
-        misc.imsave('trans/' + str(p) + '_' + res, np.reshape(image, (32, 32)))
+        print "predict: ",p
+        p = symMap[str(p[0])]
+        print the_file
+        g = re.match(".*eq\d*_(.*)_\d*_\d*_\d*_\d*.png", the_file)
+        if g:
+            res = g.group(1)
+        else:
+            continue
+        print "res: ",res,type(res)
+        print "p: ", p,type(p)
+        misc.imsave('trans/' + str(p) + '_' + res + ".png", np.reshape(image, (32, 32)))
         if p == res:
             acc += 1
         else:
-            misc.imsave('err/' + str(p) + '_' + res,
+            misc.imsave('err/' + str(p) + '_' + res + ".png",
                         np.reshape(image, (32, 32)))
         fid += 1.
-        if count == 10:
+        if count >= 10:
             break
     print(acc, fid, acc / fid)
